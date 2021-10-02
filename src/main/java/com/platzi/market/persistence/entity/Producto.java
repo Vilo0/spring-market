be83bec0d1d6aed21a76a08b2e341be0a5b2a508
Edgard Vilo <evilo@hostyweb.cl>
2021-10-02 15:25:51 -0300

    clase 17

--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -5,13 +5,16 @@ import javax.persistence.*;
 @Entity
 @Table(name = "productos")
 public class Producto {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Integer idProducto;
 
     private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    private Categoria categoria;
 
     @Column(name = "id_categoria")
     private Integer idCategoria;
