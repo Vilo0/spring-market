bda33806bf9c05d16f7c4a1fa270853433992d49
Edgard Vilo <evilo@hostyweb.cl>
2021-10-09 11:21:54 -0300

    clase 29

---
+++ src/main/java/com/platzi/market/domain/Purchase.java
@@ -0,0 +1,70 @@
+package com.platzi.market.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Purchase {
+    private int purchaseId;
+    private String clientId;
+    private LocalDateTime data;
+    private String paymentMethod;
+    private String comment;
+    private String status;
+    private List<PurchaseItem> item;
+
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<PurchaseItem> getItem() {
+        return item;
+    }
+
+    public void setItem(List<PurchaseItem> item) {
+        this.item = item;
+    }
+}
